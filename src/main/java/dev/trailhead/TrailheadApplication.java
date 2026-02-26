5cdd11b9a379575c131ccdbe06a8211e86bea337
Jonathan Hall <jonathan.edward.hall@gmail.com>
2026-02-26 11:12:15 -0500

    Build initial project.

---
+++ src/main/java/dev/trailhead/TrailheadApplication.java
@@ -0,0 +1,13 @@
+package dev.trailhead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailheadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailheadApplication.class, args);
+	}
+
+}
